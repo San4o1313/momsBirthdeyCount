af9d2d19672492f7de464440318985470902b7d6
San4o1313 <56685642+San4o1313@users.noreply.github.com>
2021-04-23 12:49:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let deysId = document.getElementById("deys");	
+let hoursId = document.getElementById("hours");	
+let minetsId = document.getElementById("minets");	
+let secondsId = document.getElementById("seconds");	
+
+let targetDate = new Date(`20 may ${new Date().getFullYear()}` )
+
+
+function tic() {
+	let diff = Math.floor((targetDate - new Date())/1000);
+	let deys = Math.floor(diff/3600/24);
+	let hours =Math.floor(diff/3600)%24 ;
+	let minets = Math.floor(diff/60)%60;
+	let seconds = diff%(60);
+
+	function formatDate(dt) {
+		return (dt < 10) ? ":0"+dt : ":"+dt; 
+		
+	}
+	
+	// console.log(deys, hours, minets, seconds)
+
+	deysId.innerHTML = deys;
+	hoursId.innerHTML = formatDate(hours);
+	minetsId.innerHTML = formatDate(minets);
+	secondsId.innerHTML = formatDate(seconds);
+	
+}
+
+
+tic();
+
+setInterval(tic, 1000)
